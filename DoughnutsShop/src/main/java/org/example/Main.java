372804491e8fb4d74b7be3ac89e1ba2f1c7c6e23
darsius <129696007+darsius@users.noreply.github.com>
2024-04-18 22:16:55 +0300

    set up hibernate

---
+++ DoughnutsShop/src/main/java/org/example/Main.java
@@ -0,0 +1,31 @@
+package org.example;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+import org.example.entities.UserAccount;
+import org.example.persistence.CustomPersistenceUnitInfo;
+import org.hibernate.jpa.HibernatePersistenceProvider;
+
+import java.util.HashMap;
+
+
+public class Main {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = new HibernatePersistenceProvider()
+                .createContainerEntityManagerFactory(new CustomPersistenceUnitInfo(), new HashMap<>());
+
+        EntityManager em = emf.createEntityManager();
+
+        em.getTransaction().begin();
+
+        UserAccount u = new UserAccount();
+        u.setUsername("User2");
+        u.setPassword("password2");
+
+        em.persist(u);
+
+        em.getTransaction().commit();
+
+    }
+}
