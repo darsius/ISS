49758b6e1739e3480f8fac46ca233462f06c1a91
darsius <p_darius211@yahoo.com>
2024-04-22 15:04:40 +0300

    handled navigation between all views

--- src/main/java/com/example/proiecto/Controller/CurrentOrderController.java
+++ src/main/java/com/example/proiecto/Controller/CurrentOrderController.java
@@ -7,6 +7,39 @@ import javafx.scene.control.Button;
 import java.io.IOException;
 
 public class CurrentOrderController extends NavigateController{
+    @FXML
+    private Button logOutButton;
+    @FXML
+    private Button goToOrdersHistoryButton;
+    @FXML
+    private Button goToMainPageButton;
 
+
+    public void initialize() {
+        logOutButton.setOnAction(event -> {
+            try {
+                switchToLogInView(event);
+            } catch (IOException e) {
+                System.err.println("Failed to load the log-in view: " + e.getMessage());
+                e.printStackTrace();
+            }
+        });
+        goToOrdersHistoryButton.setOnAction(event -> {
+            try {
+                switchOrdersHistoryView(event);
+            } catch (IOException e) {
+                System.err.println("Failed to load the current order view: " + e.getMessage());
+                e.printStackTrace();
+            }
+        });
+        goToMainPageButton.setOnAction(event -> {
+            try {
+                switchMainPageView(event);
+            } catch (IOException e) {
+                System.err.println("Failed to load the main view: " + e.getMessage());
+                e.printStackTrace();
+            }
+        });
+    }
 
 }
